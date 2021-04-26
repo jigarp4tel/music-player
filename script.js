ccf4f03d1662888df0da233765cd27512ece2b39
Jigar Patel <44246251+jigarp4tel@users.noreply.github.com>
2021-04-25 23:06:16 -0500

    -updated

--- script.js
+++ script.js
@@ -57,12 +57,26 @@ function prevSong() {
 function nextSong() {
     songIndex++
 
-    if (songIndex > (songs.length - 1) ) {
+    if (songIndex > (songs.length - 1)) {
         songIndex = 0
     }
 
     loadSong(songs[songIndex])
     playSong()
+}
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
 }
 
 
@@ -83,3 +97,7 @@ playBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
